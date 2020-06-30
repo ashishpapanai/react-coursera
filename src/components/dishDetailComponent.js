b37cea590982288f6102d0208482936797c9e3cb
Ashish Papanai <ashishpapanai00@gmail.com>
2020-06-30 09:14:58 +0530

    Fetch post comment

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -18,7 +18,7 @@ function RenderDish({ dish }) {
     );
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
     if (comments == null || comments.length == 0) {
         return (<div></div>);
     } else {
@@ -34,7 +34,7 @@ function RenderComments({ comments, addComment, dishId }) {
                     )
                     )}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         )
     }
@@ -61,7 +61,7 @@ class CommentForm extends Component {
     handleSubmit(values) {
         this.toggleModal();
         console.log()
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -169,7 +169,7 @@ const DishDetail = (props) => {
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <RenderComments comments={props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id} />
                     </div>
                 </div>
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,20 +1,21 @@
-import { COMMENTS } from '../shared/comments';
-import * as ActionTypes from './ActionsType'
-export const Comments = (state = {
-    errMess: null,
-    comments: []
-}, action) => {
-    switch (action.type) {
-        case ActionTypes.ADD_COMMENTS:
-            return { ...state, isLoading: false, errMess: null, comments: action.payload };
 
-        case ActionTypes.ADD_COMMENT:
-            var comment = action.payload;
-            comment.id = state.length;
-            comment.date = new Date().toISOString();
-            return state.concat(comment)
+import * as ActionTypes from './ActionsType';
+export const Comments = (state = { errMess: null,
+				   comments: []},
+			 action) => {
+			     switch (action.type) {
+			     case ActionTypes.ADD_COMMENT:
+				 var comment = action.payload;
+				 console.log("Comment: ", comment);
+				 return {...state, comments:state.comments.concat(comment)};
 
-        default:
-            return state;
-    }
-}
+			     case ActionTypes.ADD_COMMENTS:
+				 return {...state, errMess: null, comments: action.payload};
+				 
+			     case ActionTypes.COMMENTS_FAILED:
+				 return {...state, errMess: action.payload};
+				 
+			     default:
+				 return state;
+			     }
+			 };
